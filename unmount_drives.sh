258491738e5d24dec0372f876ec7cb3d3cf4a7a4
Ashish Kansara <ashishnkansara@gmail.com>
2019-09-10 09:35:14 -0600

    Generalise unmount script for any crypt device

--- unmount_drives.sh
+++ unmount_drives.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-echo "Checking for open files"
+echo "Checking for open files on $1"
 echo ""
 
 # fuser should check for applications with files open in the specified path
 # dump the output, we don't need to show it unless there is an application
-if fuser -m "/data" &> /dev/null; then
-    fuser -mv /data
+if fuser -m "/$1" &> /dev/null; then
+    fuser -mv /$1
 else
     DATA_OK=1
 fi
 
 if [[ $DATA_OK = 1 ]]; then
-    echo "OK to unmount drives"
+    echo "OK to unmount $1"
     echo ""
-    echo "Unmounting drives"
+    echo "Unmounting $1"
     echo ""
-    sudo umount /dev/mapper/data
-    sudo cryptsetup close data
+    sudo umount /dev/mapper/$1
+    sudo cryptsetup close $1
 else
     echo ""
-    echo "Unable to unmount drives"
+    echo "Unable to unmount $1"
 fi
 
